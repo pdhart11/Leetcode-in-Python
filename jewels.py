69144fb4aff9dafeafc02b35411583538d88ae17
pdhart11 <106359270+pdhart11@users.noreply.github.com>
2022-07-28 14:58:01 -0400

    Create jewels.py

---
+++ jewels.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        jewel_list = []
+        jewel_list.append(jewels[0])
+
+        for i in range(len(jewels)):
+            for j in range(len(stones)):
+                if jewels[i] == stones[j]:
+                    count +=1
+        return count
